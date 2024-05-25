1cec70e79941b37c1ec0ae77c69f2d2b59ca0c58
Kelly Le <kellyl17@uci.edu>
2024-05-25 10:47:34 -0700

    List, button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
-    setInterval(function () {
+
+    function updateDisplay() {
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
 
@@ -11,13 +12,25 @@ function startTimer(duration, display) {
 
         if (--timer < 0) {
             timer = 0;
-            timer = duration; // uncomment this line to reset timer automatically after reaching 0
+            // Uncomment the following line if you want to reset the timer automatically after reaching 0
+            // timer = duration;
         }
-    }, 1000);
+    }
+
+    updateDisplay(); // Initial call to update display immediately
+    setInterval(updateDisplay, 1000);
 }
 
+
+
 window.onload = function () {
-    var time = 1500, // your time in seconds here
-        display = document.querySelector('#safeTimerDisplay');
-    startTimer(time, display);
+    var time = 1500; // Your time in seconds here (25 minutes)
+    var display = document.querySelector('#safeTimerDisplay');
+    var startButton = document.querySelector('#startButton');
+
+    startButton.addEventListener('click', function () {
+        startTimer(time, display);
+        startButton.disabled = true;
+    });
+    
 };
